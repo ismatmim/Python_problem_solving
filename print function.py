8a881b7e48119efb257b419c2c2fd1101ea747ef
ismat <iftamim99@gmail.com>
2026-06-04 06:41:45 +0600

    Create print function.py
    Print the list of integers from  through  as a string, without spaces

---
+++ print function.py
@@ -0,0 +1,11 @@
+
+#Print the list of integers from  through  as a string, without spaces.
+
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for n in range(1,n+1):
+        print(n,end="")
+     
+        
